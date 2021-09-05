acbf32bfe9a99574f184f79ceb20c9dd040d3dba
Sithum Basnayaka <sithum.dashantha@gmail.com>
2021-09-05 16:12:40 +0530

    Made Separate class for handle World Time API REQ

--- lib/pages/loading.dart
+++ lib/pages/loading.dart
@@ -1,6 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:http/http.dart';
-import 'dart:convert';
 
 class Loading extends StatefulWidget {
   @override
@@ -8,32 +6,9 @@ class Loading extends StatefulWidget {
 }
 
 class _LoadingState extends State<Loading> {
-  void getTime() async {
-    // * Make the resonse
-    Response response = await get(
-        Uri.parse('http://worldtimeapi.org/api/timezone/Asia/Colombo'));
-    Map data = jsonDecode(response.body);
-    // print(data);
-
-    // * get props from data
-    String dateTime = data['datetime'];
-    String offsetHours = data['utc_offset'].substring(1, 3);
-    String offsetMinutes = data['utc_offset'].substring(4);
-    // print(dateTime);
-    // print(offsetHours);
-    // print(offsetMinutes);
-
-    // * Create Date time object
-    DateTime now = DateTime.parse(dateTime);
-    now = now.add(Duration(hours: int.parse(offsetHours)));
-    now = now.add(Duration(minutes: int.parse(offsetMinutes)));
-    print(now);
-  }
-
   @override
   void initState() {
     super.initState();
-    getTime();
   }
 
   @override
---
+++ lib/services/world_time.dart
@@ -0,0 +1,41 @@
+import 'package:http/http.dart';
+import 'dart:convert';
+
+class WordlTime {
+  String location; // * Location name of the UI
+  String? time; // * Time in that location
+  String flagURL; // * URL to an asset flag image
+  String API_URL; // * Location URL for API endpoint
+
+  WordlTime({
+    required this.location,
+    required this.flagURL,
+    required this.API_URL,
+  });
+
+  void getTime() async {
+    // * Make the resonse
+    Response response =
+        await get(Uri.parse('http://worldtimeapi.org/api/timezone/$API_URL'));
+    Map data = jsonDecode(response.body);
+
+    // * get props from data
+    String dateTime = data['datetime'];
+    String offsetHours = data['utc_offset'].substring(1, 3);
+    String offsetMinutes = data['utc_offset'].substring(4);
+
+    // * Create Date time object
+    DateTime now = DateTime.parse(dateTime);
+    now = now.add(Duration(hours: int.parse(offsetHours)));
+    now = now.add(Duration(minutes: int.parse(offsetMinutes)));
+
+    // * Set the time prop
+    time = now.toString();
+  }
+}
+
+WordlTime instance = WordlTime(
+  location: "Sri Lanka",
+  flagURL: "sl.png",
+  API_URL: "Asia/Colombo",
+);
