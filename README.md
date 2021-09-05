8110c319880aa17f0da663038cbe6e3f72d27652
Sithum Basnayaka <sithum.dashantha@gmail.com>
2021-09-05 15:12:38 +0530

    async functions combined with Future.Delayed

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # world_time
+
+
+## Stateless
+    - State doesn't change over time
+    - build functions only runs once
+
+## Stateful
+    - State CAN change over time
+    - setState() triggers the build function
--- lib/pages/choose_location.dart
+++ lib/pages/choose_location.dart
@@ -6,10 +6,43 @@ class ChooseLocation extends StatefulWidget {
 }
 
 class _ChooseLocationState extends State<ChooseLocation> {
+  void getData() async {
+    // * Simulate network request for a username
+    await Future.delayed(Duration(seconds: 3), () {
+      print('Sithum');
+    });
+
+    // * Simulate network request for the bio of a username
+    Future.delayed(Duration(seconds: 2), () {
+      print('Full Stack Developer, Online Entrepreneur');
+    });
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    print('init state runs');
+  }
+
   @override
   Widget build(BuildContext context) {
+    print('build state runs');
     return Scaffold(
-      body: Text('Choose Location'),
+      backgroundColor: Colors.grey[200],
+      appBar: AppBar(
+        title: Text('Choose a location'),
+        backgroundColor: Colors.blue[900],
+        elevation: 0.0,
+        centerTitle: true,
+      ),
+      body: ElevatedButton(
+        onPressed: () {
+          setState(() {
+            getData();
+          });
+        },
+        child: Text('get data'),
+      ),
     );
   }
 }
